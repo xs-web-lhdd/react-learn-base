fae9c89f02bd03bfc7c03f4b159dd9e83417d29a
凉风有信 <3042146237@qq.com>
2021-08-24 18:18:03 +0800

    feat:React-transition-group动画

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, { Component, Fragment } from 'react'
 import './style.css'
+// 引入动画组件
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      show: true
+      list: []
     }
-    this.handleToggle = this.handleToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
 
-  handleToggle () {
-    this.setState({
-      show: this.state.show ? false : true
+  handleAddItem () {
+    this.setState((preState) => {
+      return {
+        list: [...preState.list, 'item']
+      }
     })
   }
 
   render () {
     return (
       <Fragment>
-        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
-        <button onClick={this.handleToggle}>toggle</button>
+        {/* 单个元素： */}
+        {/* <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames="my-node"
+        >
+          <div>hello</div>
+        </CSSTransition> */}
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                timeout={1000}
+                classNames="my-node"
+                key={index}
+              >
+                <div>{item}</div>
+              </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        {/* <div className={this.state.show ? 'show' : 'hide'}>hello</div> */}
+        <button onClick={this.handleAddItem}>toggle</button>
       </Fragment>
     )
   }
