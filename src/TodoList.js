d408aa58cc5566d5ddd4a4d3cc370ea56cb19af2
凉风有信 <3042146237@qq.com>
2021-08-24 22:18:09 +0800

    feat:actionTypes代码拆分

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd';
 import store from './store/index'
+import { CHNAGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TPDO_ITEM } from './store/actionTypes'
 
 class TodoList extends Component {
   constructor(props) {
@@ -15,7 +16,7 @@ class TodoList extends Component {
 
   handleInputChange (e) {
     const action = {
-      type: 'change_input_value',
+      type: CHNAGE_INPUT_VALUE,
       value: e.target.value
     }
     store.dispatch(action)
@@ -27,14 +28,14 @@ class TodoList extends Component {
 
   handleButtonClick () {
     const action = {
-      type: 'add_todo_item'
+      type: ADD_TODO_ITEM
     }
     store.dispatch(action)
   }
 
   handleItemDelete (index) {
     const action = {
-      type: 'delete_todo_item',
+      type: DELETE_TPDO_ITEM,
       index
     }
     store.dispatch(action)
